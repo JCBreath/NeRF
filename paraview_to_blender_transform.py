f0091ff42fe7112fa4113dfdd6b29ac012eede70
Siyuan Yao <jim.charles.yao@gmail.com>
2023-04-14 10:34:17 -0400

    Add files via upload

--- paraview_to_blender_transform.py
+++ paraview_to_blender_transform.py
@@ -1,9 +1,54 @@
 import torch
-from volumetric_rendering.ray_sampler import RaySampler
-from camera_utils import LookAtPoseSampler
 import math, os
 import json
-from models.tensorBase import get_rays, get_ray_directions
+from kornia import create_meshgrid
+
+def sample_point(camera_origins, lookat_position):
+    forward_vectors = normalize_vecs(lookat_position - camera_origins)
+    return create_cam2world_matrix(forward_vectors, camera_origins)
+
+def get_ray_directions(H, W, focal, center=None):
+    """
+    Get ray directions for all pixels in camera coordinate.
+    Reference: https://www.scratchapixel.com/lessons/3d-basic-rendering/
+               ray-tracing-generating-camera-rays/standard-coordinate-systems
+    Inputs:
+        H, W, focal: image height, width and focal length
+    Outputs:
+        directions: (H, W, 3), the direction of the rays in camera coordinate
+    """
+    grid = create_meshgrid(H, W, normalized_coordinates=False)[0] + 0.5
+
+    i, j = grid.unbind(-1)
+    # the direction here is without +0.5 pixel centering as calibration is not so accurate
+    # see https://github.com/bmild/nerf/issues/24
+    cent = center if center is not None else [W / 2, H / 2]
+    directions = torch.stack([(i - cent[0]) / focal[0], (j - cent[1]) / focal[1], torch.ones_like(i)], -1)  # (H, W, 3)
+
+    return directions
+
+def get_rays(directions, c2w):
+    """
+    Get ray origin and normalized directions in world coordinate for all pixels in one image.
+    Reference: https://www.scratchapixel.com/lessons/3d-basic-rendering/
+               ray-tracing-generating-camera-rays/standard-coordinate-systems
+    Inputs:
+        directions: (H, W, 3) precomputed ray directions in camera coordinate
+        c2w: (3, 4) transformation matrix from camera coordinate to world coordinate
+    Outputs:
+        rays_o: (H*W, 3), the origin of the rays in world coordinate
+        rays_d: (H*W, 3), the normalized direction of the rays in world coordinate
+    """
+    # Rotate ray directions from camera coordinate to the world coordinate
+    rays_d = directions @ c2w[:3, :3].T  # (H, W, 3)
+    # rays_d = rays_d / torch.norm(rays_d, dim=-1, keepdim=True)
+    # The origin of all rays is the camera origin in world coordinate
+    rays_o = c2w[:3, 3].expand(rays_d.shape)  # (H, W, 3)
+
+    rays_d = rays_d.view(-1, 3)
+    rays_o = rays_o.view(-1, 3)
+
+    return rays_o, rays_d
 
 def normalize_vecs(vectors: torch.Tensor) -> torch.Tensor:
     """
@@ -62,7 +107,7 @@ for filename in img_files:
     z = float(z)*r
 
     camera_origins = torch.FloatTensor([x,y,z]).view(1,3)  * 4.0311
-    forward_cam2world_pose = LookAtPoseSampler.sample_point(camera_origins, camera_pivot)
+    forward_cam2world_pose = sample_point(camera_origins, camera_pivot)
     c2w = forward_cam2world_pose
 
     swap_row = torch.FloatTensor([[1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])
